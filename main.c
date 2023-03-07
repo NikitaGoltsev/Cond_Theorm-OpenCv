fc9efc665c4624e3f346f97ebea658118e5bbe6b
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-07 23:12:11 +0700

    without_prag

--- main.c
+++ main.c
@@ -18,7 +18,9 @@ int main(int argc, char *argv[]) {
   } else {
   }
   arr = creater_mtr(n, m);
-    
+
+  cicle_of_prog(arr, n, m, iter_max);
+
   clear(arr, n);
   return 0;
 }
@@ -50,7 +52,7 @@ void clear(int **mas, int n) {
 void cicle_of_prog(int **mas, int n, int m, int iter_max) {
   int iter = 0;
   int error_c = 0; // Is its init here?
-    int toll;
+  int toll;
   while (iter <= iter_max && error_c > toll) {
     iter += 1;
     error_c = 0;
@@ -60,21 +62,23 @@ void cicle_of_prog(int **mas, int n, int m, int iter_max) {
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         //
-        local_arr[i][j] = (mas[i][j] + mas[i-1][j] + mas[i][j-1] + mas[i+1][j] + mas[i][j+1]);
+        local_arr[i][j] = (mas[i][j] + mas[i - 1][j] + mas[i][j - 1] +
+                           mas[i + 1][j] + mas[i][j + 1]);
         // add error check
         error_c = max_fn(error_c, (local_arr[i][j] - mas[i][j]));
-
       }
     }
 
     // Iter check, out of cicles
-    if (iter % 100 == 0 || iter == 1)printf("\n");
-      clear(local_arr, n);
+    if (iter % 100 == 0 || iter == 1)
+      printf("\n");
+    clear(local_arr, n);
     break;
   }
 }
 
-int max_fn(int x, int y){
-    if(x > y) y = x;
-    return y;
+int max_fn(int x, int y) {
+  if (x > y)
+    y = x;
+  return y;
 }
