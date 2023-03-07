bc3423a4efdf5e87e47183e55a6fe432799e6336
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-07 21:12:59 +0700

    avg_save

--- main.c
+++ main.c
@@ -2,16 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ITER_MAX 100000
-
 int **creater_mtr(int n, int m);
 void clear(int **mas, int n);
-void cicle_of_prog(int **mas, int n, int m);
+void cicle_of_prog(int **mas, int n, int m, int iter_max);
 
 int main(int argc, char *argv[]) {
   int **arr;
   int n, m;
-  scanf("%d %d", &n, &m);
+  int iter_max;
+
+  if (argc <= 1) {
+    scanf("%d %d", &n, &m);
+    scanf("%d", &iter_max);
+  } else {
+  }
   arr = creater_mtr(n, m);
 
   clear(arr, n);
@@ -42,13 +46,19 @@ void clear(int **mas, int n) {
   free(mas);
 }
 
-void cicle_of_prog(int **mas, int n, int m) {
+void cicle_of_prog(int **mas, int n, int m, int iter_max) {
   int iter = 0;
 
-  while (iter <= ITER_MAX) {
+  while (iter <= iter_max) {
     iter += 1;
     int **local_arr;
     local_arr = creater_mtr(n, m);
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        // local_arr[i][j] = mas[][] + mas[][] + mas[][] + mas[] + mas[][];
+      }
+    }
 
     clear(local_arr, n);
     break;
