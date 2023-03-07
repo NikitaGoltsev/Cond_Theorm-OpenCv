389ca1da0cdce983d6aad1831aa6802718eeac00
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-07 20:35:46 +0700

    avg_save

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int **creater_mtr(int n, int m);
+void clear(int **mas, int n);
 
 int main(int argc, char *argv[]) {
   int **arr;
@@ -13,22 +15,27 @@ int main(int argc, char *argv[]) {
 int **creater_mtr(int n, int m) {
   int **mas;
   mas = malloc(sizeof(int *) * n);
-
   // Cicle here
-
   for (int i = 0; i < n; i++) {
     mas[i] = malloc(sizeof(int) * m);
+    for (int j = 0; j < m; j++)
+      mas[i][j] = 0;
   }
-
   mas[0][0] = 10;
   mas[0][m - 1] = 20;
   mas[n - 1][0] = 30;
   mas[n - 1][m - 1] = 20;
+
   return mas;
 }
 
-/*
-void clear(int *mas) {}
+void clear(int **mas, int n) {
+  for (int i = 0; i < n; i++) {
+    free(mas[i]);
+  }
+  free(mas);
+}
 
+/*
 void cicle_of_prog() {}
 */
