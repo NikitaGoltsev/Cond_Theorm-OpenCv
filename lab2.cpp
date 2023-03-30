8979a539f5be5d462da13e96fc75719f264d0803
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-31 06:40:13 +0700

    Rebuild of all

---
+++ lab2.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+#define LF_SUP
+
+#ifdef LF_SUP 
+#define TYPE double
+#define ABS fabs
+#define MAX fmax
+#define CAST std::stod
+#else
+#define TYPE float
+#define ABS fabsf
+#define MAX fmaxf
+#define CAST std::stof
+#endif
+
+
+int initArr(TYPE** A,int n){
+
+    A[0][0]=10.0;
+    A[0][n-1] = 20.0;
+    A[n-1][0]=30.0;
+    A[n-1][n-1]=20.0;
+
+    for (int i{1};i<n-1;++i){
+        A[0][i]=10+((A[0][n-1] - A[0][0])/ (n-1))*i;
+        A[i][0]=10+((A[n-1][0] - A[0][0])/ (n-1))*i;
+        A[n-1][i]=10+((A[n-1][0] - A[n-1][n-1])/ (n-1))*i;
+        A[i][n-1]=10+((A[n-1][n-1] - A[n-1][0])/ (n-1))*i;
+    }
+
+    return 1;
+}
+
+void solution(TYPE tol,int iter_max,int n){
+    TYPE error {1.0};
+    int iter{0};
+    TYPE** A = new TYPE*[n],**Anew = new TYPE*[n]; 
+    for ( int i {0}; i < n; ++i){
+        A[i] = new TYPE[n];
+        Anew[i] = new TYPE[n];
+    }
+
+    initArr(A,n);
+    initArr(Anew,n);
+        
+    while (error > tol && iter < iter_max){
+        error = 0.0;
+        for (int j {1}; j < n - 1; ++j){
+            for (int i {1}; i < n - 1; ++i){
+                Anew[j][i] = 0.25 * (A[j][i + 1] + A[j][i - 1] + A[j - 1][i] + A[j + 1][i]);
+                error = MAX(error, ABS(Anew[j][i] - A[j][i]));
+            }
+        }
+
+        /*TYPE** temp = A;
+        A = Anew;
+        Anew = temp;*/
+
+        swap_for_tp(A, Anew);
+
+        ++iter;
+    }
+    
+    std::cout<<"Iterations: "<<iter<<std::endl<<"Error: "<<error<<std::endl;
+
+    for (int i {0}; i < n; i++){
+        delete[] A[i];
+        delete[] Anew[i];
+    }
+    delete [] A;
+    delete [] Anew;
+}
+
+int main(int argc, char *argv[]){
+    TYPE tol{1e-6};
+    int iter_max{1000000},n{128}; //значения для отладки, по умолчанию инициализировать нулями
+
+    std::string tmpStr;
+    //-t - точность
+    //-n - размер сетки
+    //-i - кол-во итераций
+    for (int i{1};i<argc;++i){
+        tmpStr = argv[i];
+        if(!tmpStr.compare("-t")){
+            tol = CAST(argv[i + 1]);
+            ++i;
+        }
+
+        if(!tmpStr.compare("-i")) {
+            iter_max = std::stoi(argv[i + 1]);
+            ++i;
+        }
+
+        if(!tmpStr.compare("-n")) {
+            n = std::stoi(argv[i + 1]);
+            ++i;
+        } 
+    }
+    solution(tol,iter_max,n);
+}
+
+void swap_for_tp(TYPE **x, TYPE **y){
+    TYPE t;
+    t = **x;
+    **x = **y;
+    **y = t;
+}
